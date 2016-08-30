a78b438f1bf6282b302dca98fbf184542595361d
suresh07 <soori_rozee@yahoo.com>
2016-08-30 16:37:53 +0530

    Commit No. 3 - Features List enabled

--- php/include_header.php
+++ php/include_header.php
@@ -43,7 +43,7 @@
 					<li><a href="volumes.php"><i class="fa fa-book"></i> ಸಂಪುಟಗಳು</a></li>
 					<li><a href="articles.php"><i class="fa fa-files-o"></i> ಲೇಖನಗಳು</a></li>
 					<li><a href="authors.php"><i class="fa fa-users"></i> ಲೇಖಕರು</a></li>
-					<li><a href="#"><i class="fa fa-tags"></i> ಸ್ಥಿರ ಶೀರ್ಷಿಕೆಗಳು</a></li>
+					<li><a href="features.php"><i class="fa fa-tags"></i> ಸ್ಥಿರ ಶೀರ್ಷಿಕೆಗಳು</a></li>
 					<li><a href="search.php"><i class="fa fa-search"></i> ಹುಡುಕಾಟ</a></li>
 				</ul>
 			</nav> <!-- cd-sec-main-nav -->
